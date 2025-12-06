d0b0f56ff7e7cea2589f2000211cd54f65fd45e2
adit <adityavivaldi26@gmail.com>
2025-12-07 00:27:07 +0800

    doing controller and some views

---
+++ src/main/java/com/controller/MentalHealthLiteracyController.java
@@ -0,0 +1,18 @@
+package com.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequestMapping("/literacy")
+
+public class MentalHealthLiteracyController{
+    @GetMapping
+    public String showLiteracyPage(Model model) {
+        return "Mental Health Literacy Module/literacyPage";
+    }
+         
+}
---
+++ src/main/java/com/controller/PeerSupportController.java
@@ -0,0 +1,26 @@
+package com.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/peer")
+public class PeerSupportController {
+    
+    @GetMapping("/posts")
+    public String showPostsPage(Model model){
+        return "peerSupportModule/posts";
+    }
+
+    @GetMapping("/createPost")
+    public String createPostPage(Model model){
+        return "peerSupportModule/createPost";
+    }
+
+    @GetMapping("/commentPost")
+    public String showCommentPage(Model model){
+        return "peerSupportModule/commentPost";
+    }
+}

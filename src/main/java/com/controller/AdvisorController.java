fdeb499e37d8ec3fa9681fefa3d70689e8d47094
adit <adityavivaldi26@gmail.com>
2025-12-08 01:34:24 +0800

    added controller for advisor

---
+++ src/main/java/com/controller/AdvisorController.java
@@ -0,0 +1,20 @@
+package com.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.model.User;
+
+@Controller
+@RequestMapping("/advisor")
+public class AdvisorController {
+    
+    @GetMapping(value = {"", "/"})
+    public String showStudentLandingPage(Model model) {
+        // User user = userService.getUserById(CURRENT_USER_ID);
+        // model.addAttribute("advisorName", user != null ? user.getName() : "Student");
+        return "mainPages/advisorLandingPage";
+    }
+}

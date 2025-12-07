6120e869cd6f934569d8ee90a9d086e51f71c357
adit <adityavivaldi26@gmail.com>
2025-12-08 01:50:25 +0800

    finished advisor landing page

--- src/main/java/com/controller/AdvisorController.java
+++ src/main/java/com/controller/AdvisorController.java
@@ -5,8 +5,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.model.User;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller

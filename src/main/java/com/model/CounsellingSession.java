1638a5924cbd066d5c2c750573cadc64dd16491c
Mohamed-M-M-Ramadan <167596723+Mohamed-M-M-Ramadan@users.noreply.github.com>
2026-01-13 17:00:38 +0800

    added counseling sesson entity

--- src/main/java/com/model/CounsellingSession.java
+++ src/main/java/com/model/CounsellingSession.java
@@ -7,40 +7,40 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "appointments") // SQL table name
-public class Appointment {
+@Table(name = "counselling_sessions") // SQL table name
+public class CounsellingSession {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String date;
     private String time;
-    private String counselor;
-    private String reason;
+    private String advisor;
+    private String name;
 
     // Constructors
-    public Appointment() {}
+    public CounsellingSession() {}
 
-    public Appointment(String date, String time, String counselor, String reason) {
+    public CounsellingSession(String date, String time, String advisor, String name) {
         this.date = date;
         this.time = time;
-        this.counselor = counselor;
-        this.reason = reason;
+        this.advisor = advisor;
+        this.name = name;
     }
 
-    // Getters and Setters (Important for Hibernate to access data)
+    // Getters and Setters
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
-    
+
     public String getDate() { return date; }
     public void setDate(String date) { this.date = date; }
-    
+
     public String getTime() { return time; }
     public void setTime(String time) { this.time = time; }
+
+    public String getAdvisor() { return advisor; }
+    public void setAdvisor(String advisor) { this.advisor = advisor; }
     
-    public String getCounselor() { return counselor; }
-    public void setCounselor(String counselor) { this.counselor = counselor; }
-    
-    public String getReason() { return reason; }
-    public void setReason(String reason) { this.reason = reason; }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
 }
